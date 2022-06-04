694cf76e8c2f0a341ce330756a866226aca6bf19
lihaitao <lihaitao.me@qq.com>
2022-06-04 21:22:07 +0800

    修改

---
+++ js/clock-in.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	//eg: pattern = "yyyy-MM-dd hh:mm:ss";
+	Date.prototype.getFormattedDate = function(pattern) {
+	    
+	    function getFullStr(i) {
+	        return i > 9 ? "" + i : "0" + i;
+	    }
+	    
+	    pattern = pattern.replace(/yyyy/,this.getFullYear())
+	        .replace(/MM/,getFullStr(this.getMonth()+1))
+	        .replace(/dd/,getFullStr(this.getDate()))
+	        .replace(/hh/,getFullStr(this.getHours()))
+	        .replace(/mm/,getFullStr(this.getMinutes()))
+	        .replace(/ss/,getFullStr(this.getSeconds()));
+
+	    return pattern;
+	};
+
+	$('.header').text((new Date()).getFormattedDate("yyyy/MM/dd hh:mm:ss"));
+	setInterval(function() {
+		$('.header').text((new Date()).getFormattedDate("yyyy/MM/dd hh:mm:ss"));
+	}, 1000);
+
+
+
+	
+
+});
